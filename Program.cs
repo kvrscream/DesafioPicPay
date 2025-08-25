184943c33b69c5a630ee5008de05d06f1201380c
Felipe Botelho de Souza <botelho422@gmail.com>
2025-08-25 08:30:43 -0300

    Implementando services entre controllers e reposotories. Falta implentar as interfaces dos services

--- Program.cs
+++ Program.cs
@@ -20,8 +20,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // EF core
-builder.Services.AddDbContext<ApplicationDbContext>(options => 
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))    
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
@@ -29,6 +29,8 @@ builder.Services.AddScoped<IAccountRepository, AccountRepository>();
 builder.Services.AddScoped<ITransferRepository, TransferRepository>();
 
 builder.Services.AddScoped<TransferServices>();
+builder.Services.AddScoped<AccountServices>();
+builder.Services.AddScoped<UserServices>();
 
 var app = builder.Build();
 
---
+++ Services/UserServices.cs
@@ -0,0 +1,41 @@
+using estudoRepository.dtos;
+using estudoRepository.dtos.UserDTOs;
+using estudoRepository.Interfaces;
+using estudoRepository.Models;
+
+namespace estudoRepository.Services;
+
+public class UserServices : IUserRepository
+{
+  private readonly IUserRepository _repository;
+
+  public UserServices(IUserRepository repository)
+  {
+    _repository = repository;
+  }
+
+  public async Task<User> AddUser(UserDTO user)
+  {
+    return await _repository.AddUser(user: user);
+  }
+
+  public async Task<bool> DeleteUser(int id)
+  {
+    return await _repository.DeleteUser(id: id);
+  }
+
+  public async Task<List<User>> GetAll()
+  {
+    return await _repository.GetAll();
+  }
+
+  public async Task<User> GetById(int id)
+  {
+    return await _repository.GetById(id: id);
+  }
+
+  public async Task<User> UpdateUser(UserUpdateDTO user)
+  {
+    return await _repository.UpdateUser(user: user);
+  }
+}
